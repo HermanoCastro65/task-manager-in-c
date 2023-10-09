e73af0facf7ab0a57153b010b15cdbbc1e448f4b
Hermano Castro <hermanocastro18@gmail.com>
2023-10-09 15:29:35 -0300

    formate all codes

--- interfaces/buttons.c
+++ interfaces/buttons.c
@@ -1,17 +1,17 @@
 #include "interfaces.h"
 
-void on_add_button_clicked(GtkWidget *widget, gpointer data) {
-    g_print("Add Task\n");
+void on_add_button_clicked(GtkWidget * widget, gpointer data) {
+  g_print("Add Task\n");
 }
 
-void on_list_todo_button_clicked(GtkWidget *widget, gpointer data) {
-    g_print("List To-do Tasks\n");
+void on_list_todo_button_clicked(GtkWidget * widget, gpointer data) {
+  g_print("List To-do Tasks\n");
 }
 
-void on_list_completed_button_clicked(GtkWidget *widget, gpointer data) {
-    g_print("List Completes Tasks\n");
+void on_list_completed_button_clicked(GtkWidget * widget, gpointer data) {
+  g_print("List Completes Tasks\n");
 }
 
-void on_remove_button_clicked(GtkWidget *widget, gpointer data) {
-    g_print("Remove Task\n");
+void on_remove_button_clicked(GtkWidget * widget, gpointer data) {
+  g_print("Remove Task\n");
 }
--- main.c
+++ main.c
@@ -1,47 +1,41 @@
 #include "interfaces/interfaces.h"
 
-GtkWidget *window;
-GtkWidget *addButton;
-GtkWidget *listToDoButton;
-GtkWidget *listCompletedButton;
-GtkWidget *removeButton;
+GtkWidget * window;
+GtkWidget * addButton;
+GtkWidget * listToDoButton;
+GtkWidget * listCompletedButton;
+GtkWidget * removeButton;
 
-int main(int argc, char *argv[]) {
-    gtk_init(&argc, &argv);
+int main(int argc, char * argv[]) {
+  gtk_init( & argc, & argv);
 
-    // Cria a janela principal
-    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Task Manager");
-    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
-    gtk_widget_set_size_request(window, 300, 200);
-    g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(window), "Task Manager");
+  gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+  gtk_widget_set_size_request(window, 300, 200);
+  g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    // Cria botões
-    addButton = gtk_button_new_with_label("Add Task");
-    listToDoButton = gtk_button_new_with_label("List To Do Tasks");
-    listCompletedButton = gtk_button_new_with_label("List Completed Tasks");
-    removeButton = gtk_button_new_with_label("Remove Task");
+  addButton = gtk_button_new_with_label("Add Task");
+  listToDoButton = gtk_button_new_with_label("List To Do Tasks");
+  listCompletedButton = gtk_button_new_with_label("List Completed Tasks");
+  removeButton = gtk_button_new_with_label("Remove Task");
 
-    // Conecta os sinais de clique dos botões aos callbacks
-    g_signal_connect(addButton, "clicked", G_CALLBACK(on_add_button_clicked), NULL);
-    g_signal_connect(listToDoButton, "clicked", G_CALLBACK(on_list_todo_button_clicked), NULL);
-    g_signal_connect(listCompletedButton, "clicked", G_CALLBACK(on_list_completed_button_clicked), NULL);
-    g_signal_connect(removeButton, "clicked", G_CALLBACK(on_remove_button_clicked), NULL);
+  g_signal_connect(addButton, "clicked", G_CALLBACK(on_add_button_clicked), NULL);
+  g_signal_connect(listToDoButton, "clicked", G_CALLBACK(on_list_todo_button_clicked), NULL);
+  g_signal_connect(listCompletedButton, "clicked", G_CALLBACK(on_list_completed_button_clicked), NULL);
+  g_signal_connect(removeButton, "clicked", G_CALLBACK(on_remove_button_clicked), NULL);
 
-    // Cria um layout de caixa vertical para organizar os botões
-    GtkWidget *vbox = gtk_vbox_new(TRUE, 5);
-    gtk_box_pack_start(GTK_BOX(vbox), addButton, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(vbox), listToDoButton, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(vbox), listCompletedButton, TRUE, TRUE, 0);
-    gtk_box_pack_start(GTK_BOX(vbox), removeButton, TRUE, TRUE, 0);
+  GtkWidget * vbox = gtk_vbox_new(TRUE, 5);
+  gtk_box_pack_start(GTK_BOX(vbox), addButton, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), listToDoButton, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), listCompletedButton, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), removeButton, TRUE, TRUE, 0);
 
-    // Adiciona a caixa de layout à janela principal
-    gtk_container_add(GTK_CONTAINER(window), vbox);
+  gtk_container_add(GTK_CONTAINER(window), vbox);
 
-    // Exibe todos os widgets
-    gtk_widget_show_all(window);
+  gtk_widget_show_all(window);
 
-    gtk_main();
+  gtk_main();
 
-    return 0;
+  return 0;
 }

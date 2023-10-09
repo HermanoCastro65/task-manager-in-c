996ddf70000bbba0621b3eab65cb4d71a207eec5
Hermano Castro <hermanocastro18@gmail.com>
2023-10-09 16:03:36 -0300

    change name of the header

--- interfaces/buttons.h
+++ interfaces/buttons.h
@@ -4,6 +4,7 @@
 #include <gtk/gtk.h>
 
 void on_add_button_clicked(GtkWidget * widget, gpointer data);
+void on_confirm_button_clicked(GtkWidget * widget, gpointer data);
 void on_list_todo_button_clicked(GtkWidget * widget, gpointer data);
 void on_list_completed_button_clicked(GtkWidget * widget, gpointer data);
 void on_remove_button_clicked(GtkWidget * widget, gpointer data);

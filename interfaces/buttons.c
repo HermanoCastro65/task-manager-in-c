f5bc7f20b3697387e39448f4f3dfa527e0984b29
Hermano Castro <hermanocastro18@gmail.com>
2023-10-09 16:03:19 -0300

    create a new windw in add button

--- interfaces/buttons.c
+++ interfaces/buttons.c
@@ -1,7 +1,42 @@
-#include "interfaces.h"
+#include "buttons.h"
+
+#include "../task/task.h"
+
+GtkWidget * descriptionEntry;
+GtkWidget * dateEntry;
+GtkWidget * addTaskWindow;
 
 void on_add_button_clicked(GtkWidget * widget, gpointer data) {
-  g_print("Add Task\n");
+  addTaskWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(addTaskWindow), "Add Task");
+  gtk_container_set_border_width(GTK_CONTAINER(addTaskWindow), 10);
+
+  descriptionEntry = gtk_entry_new();
+  dateEntry = gtk_entry_new();
+
+  GtkWidget * confirmButton = gtk_button_new_with_label("Confirm");
+  g_signal_connect(confirmButton, "clicked", G_CALLBACK(on_confirm_button_clicked), NULL);
+
+  GtkWidget * vbox = gtk_vbox_new(TRUE, 5);
+  gtk_box_pack_start(GTK_BOX(vbox), descriptionEntry, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), dateEntry, TRUE, TRUE, 0);
+  gtk_box_pack_start(GTK_BOX(vbox), confirmButton, TRUE, TRUE, 0);
+
+  gtk_container_add(GTK_CONTAINER(addTaskWindow), vbox);
+
+  gtk_widget_show_all(addTaskWindow);
+}
+
+void on_confirm_button_clicked(GtkWidget * widget, gpointer data) {
+  const gchar * description = gtk_entry_get_text(GTK_ENTRY(descriptionEntry));
+  const gchar * date = gtk_entry_get_text(GTK_ENTRY(dateEntry));
+
+  strcpy(tasks[taskCount].description, description);
+  strcpy(tasks[taskCount].date, date);
+  tasks[taskCount].completed = 0;
+  taskCount++;
+
+  gtk_widget_destroy(addTaskWindow);
 }
 
 void on_list_todo_button_clicked(GtkWidget * widget, gpointer data) {
